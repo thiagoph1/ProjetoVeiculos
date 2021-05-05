dd0a5c3a65939bdd15456223feaa5591dfd5b212
Thiago Pessanha <tiagofelipe15@gmail.com>
2021-05-05 16:09:06 -0300

    criação de editar e excluir serviço

--- src/main/java/com/projeto/veiculos/controllers/ServicoController.java
+++ src/main/java/com/projeto/veiculos/controllers/ServicoController.java
@@ -1,8 +1,11 @@
 package com.projeto.veiculos.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,6 +48,27 @@ public class ServicoController {
 	@PostMapping("/salvarServico")
 	public ModelAndView salvarServico(Servico servico) {
 		servicoRp.save(servico);
+		ModelAndView mav = new ModelAndView("servicos/servico");
+		Iterable<Servico> servicos = servicoRp.findAll();
+		mav.addObject("veiculos", veiculoRp.findAll());
+		mav.addObject("usuarios", usuarioRp.findAll());
+		mav.addObject("servicos", servicos);
+		return mav;
+	}
+	
+	@GetMapping("/editarServico/{idservico}")
+	public ModelAndView editarServico(@PathVariable("idservico") Long idservico) {
+		ModelAndView mav = new ModelAndView("servicos/cadastroServico");
+		Optional<Servico> servico = servicoRp.findById(idservico);
+		mav.addObject("servico", servico.get());
+		mav.addObject("veiculos", veiculoRp.findAll());
+		mav.addObject("usuarios", usuarioRp.findAll());
+		return mav;
+	}
+	
+	@GetMapping("/removerServico/{idservico}")
+	public ModelAndView excluirServico(@PathVariable("idservico") Long idservico) {
+		servicoRp.deleteById(idservico);
 		ModelAndView mav = new ModelAndView("servicos/servico");
 		Iterable<Servico> servicos = servicoRp.findAll();
 		mav.addObject("veiculos", veiculoRp.findAll());
